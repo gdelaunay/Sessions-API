cec78aa2a5f22f09ea7d28cafb3e48b636e30583
GDelaunay <goulwendelaunay@gmail.com>
2025-06-30 23:23:28 +0200

    fix commit

--- SurfSessions-API/DTOs/DailyForecast.cs
+++ SurfSessions-API/DTOs/DailyForecast.cs
@@ -1,32 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace SurfSessions_API.DTOs;
+﻿namespace SurfSessions_API.DTOs;
 
 public class DailyForecast
 {
-    public double latitude { get; set; }
-    public double longitude { get; set; }
-    public double generationtime_ms { get; set; }
-    public int utc_offset_seconds { get; set; }
-    public string timezone { get; set; }
-    public string timezone_abbreviation { get; set; }
-    public double elevation { get; set; }
-    public DailyUnits daily_units { get; set; }
-    public Daily daily { get; set; }
-}
+    public DailyForecast(List<string> dates, List<float> waveHeights, List<int> waveDirections, List<float> wavePeriods)
+    {
+        Dates = dates;
+        WaveHeights = waveHeights;
+        WaveDirections = waveDirections;
+        WavePeriods = wavePeriods;
+    }
 
-public class DailyUnits
-{
-    public string time { get; set; }
-    public string wave_height_max { get; set; }
-    public string wave_direction_dominant { get; set; }
-    public string wave_period_max { get; set; }
-}
-
-public class Daily
-{
-    public List<string> time { get; set; }
-    public List<double> wave_height_max { get; set; }
-    public List<int> wave_direction_dominant { get; set; }
-    public List<double> wave_period_max { get; set; }
+    public List<string> Dates { get; set; }
+    public List<float> WaveHeights { get; set; }
+    public List<int> WaveDirections { get; set; }
+    public List<float> WavePeriods { get; set; }
 }
