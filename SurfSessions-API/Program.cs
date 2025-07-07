3cbe63e7f0abb0b2c8dc5bd837087940f3ce40a5
GDelaunay <goulwendelaunay@gmail.com>
2025-07-08 01:24:16 +0200

    fix CORS wildcard (subdomains)

--- SurfSessions-API/Program.cs
+++ SurfSessions-API/Program.cs
@@ -42,6 +42,7 @@ builder.Services.AddCors(options =>
             policy.WithOrigins("http://localhost:4200",
                     "http://*.gdelaunay.fr",
                     "https://*.gdelaunay.fr")
+                .SetIsOriginAllowedToAllowWildcardSubdomains()
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .WithExposedHeaders("Location");
