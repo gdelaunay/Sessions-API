d4304cda840a20fd0a3883455881e6f16eb41ac5
GDelaunay <goulwendelaunay@gmail.com>
2025-07-13 01:40:12 +0200

    add migrations EF

---
+++ SurfSessions-API/Migrations/20250703214752_initialCreate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SurfSessions_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Forecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    DateTime = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    WeatherCode = table.Column<int>(type: "int", nullable: false),
+                    Temperature = table.Column<float>(type: "float", nullable: false),
+                    WaveHeight = table.Column<float>(type: "float", nullable: false),
+                    WaveDirection = table.Column<int>(type: "int", nullable: false),
+                    WavePeriod = table.Column<float>(type: "float", nullable: false),
+                    WindSpeed = table.Column<float>(type: "float", nullable: false),
+                    WindDirection = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forecasts", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Spots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    Latitude = table.Column<float>(type: "float", nullable: false),
+                    Longitude = table.Column<float>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spots", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    SpotId = table.Column<int>(type: "int", nullable: false),
+                    ForecastId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: true),
+                    Comment = table.Column<string>(type: "varchar(800)", maxLength: 800, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Forecasts_ForecastId",
+                        column: x => x.ForecastId,
+                        principalTable: "Forecasts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Spots_SpotId",
+                        column: x => x.SpotId,
+                        principalTable: "Spots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_ForecastId",
+                table: "Sessions",
+                column: "ForecastId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_SpotId",
+                table: "Sessions",
+                column: "SpotId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "Forecasts");
+
+            migrationBuilder.DropTable(
+                name: "Spots");
+        }
+    }
+}
