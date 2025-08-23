8eee90353a46c7905bbec2af9eeeb1d95db38ed5
gdelaunay <goulwendelaunay@gmail.com>
2025-08-24 00:26:49 +0200

    dev user-specific data (spots et sessions)

---
+++ SurfSessions-API/Migrations/20250823220919_addOwnerId.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurfSessions_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOwnerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Spots",
+                type: "varchar(42)",
+                maxLength: 42,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Sessions",
+                type: "varchar(42)",
+                maxLength: 42,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Spots");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Sessions");
+        }
+    }
+}
